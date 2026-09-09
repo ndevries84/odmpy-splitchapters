286e7f7bb6b038d8dcf2f4a563ac27618551b414
NateVan <10392137+ndevries84@users.noreply.github.com>
2026-09-09 07:42:20 -0700

    Add OverDrive client and Libby error types

---
+++ odmpy/libby_errors.py
@@ -0,0 +1,107 @@
+# Copyright (C) 2023 github.com/ping
+#
+# This file is part of odmpy.
+#
+# odmpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# odmpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with odmpy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import json
+from http import HTTPStatus
+
+import requests as requests
+
+#
+# For use with LibbyClient
+#
+
+
+class ClientError(Exception):
+    """Generic error class, catch-all for most client issues."""
+
+    def __init__(
+        self,
+        msg: str,
+        http_status: int = 0,
+        error_response: str = "",
+    ):
+        self.http_status = http_status or 0
+        self.error_response = error_response
+        try:
+            self.error_response_obj = json.loads(self.error_response)
+        except ValueError:
+            self.error_response_obj = {}
+        super(ClientError, self).__init__(msg)
+
+    @property
+    def msg(self):
+        return self.args[0]
+
+    def __str__(self):
+        return (
+            f"<{type(self).__module__}.{type(self).__name__}; http_status={self.http_status}, "
+            f"msg='{self.msg}', error_response='{self.error_response}''>"
+        )
+
+
+class ClientConnectionError(ClientError):
+    """Connection error"""
+
+
+class ClientTimeoutError(ClientError):
+    """Timeout error"""
+
+
+class ClientBadRequestError(ClientError):
+    """Raised when an HTTP 400 response is received."""
+
+
+class ErrorHandler(object):
+    @staticmethod
+    def process(http_err: requests.HTTPError) -> None:
+        """
+        Try to process an HTTP error from the api appropriately.
+
+        :param http_err: requests.HTTPError instance
+        :raises ClientError:
+        :return:
+        """
+        # json response
+        if (
+            http_err.response.status_code == HTTPStatus.BAD_REQUEST
+            and http_err.response.headers.get("content-type", "").startswith(
+                "application/json"
+            )
+        ):
+            error = http_err.response.json()
+            if error.get("result", "") == "upstream_failure":
+                upstream = error.get("upstream", {})
+                if upstream:
+                    raise ClientBadRequestError(
+                        msg=f'{upstream.get("userExplanation", "")} [errorcode: {upstream.get("errorCode", "")}]',
+                        http_status=http_err.response.status_code,
+                        error_response=http_err.response.text,
+                    ) from http_err
+
+                raise ClientBadRequestError(
+                    msg=str(error),
+                    http_status=http_err.response.status_code,
+                    error_response=http_err.response.text,
+                ) from http_err
+
+        # final fallback
+        raise ClientError(
+            msg=str(http_err),
+            http_status=http_err.response.status_code,
+            error_response=http_err.response.text,
+        ) from http_err
---
+++ odmpy/overdrive.py
@@ -0,0 +1,197 @@
+# Copyright (C) 2023 github.com/ping
+#
+# This file is part of odmpy.
+#
+# odmpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# odmpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with odmpy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+from typing import Optional, Dict, List
+from urllib.parse import urljoin
+
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+#
+# Basic skeletal client for the OverDrive Thunder API
+#
+
+USER_AGENT = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/605.1.15 (KHTML, like Gecko) "  # noqa
+    "Version/14.0.2 Safari/605.1.15"
+)
+SITE_URL = "https://libbyapp.com"
+THUNDER_API_URL = "https://thunder.api.overdrive.com/v2/"
+CLIENT_ID = "dewey"
+
+
+class OverDriveClient(object):
+    """
+    A really simplified OverDrive Thunder API client
+    """
+
+    def __init__(self, **kwargs) -> None:
+        """
+        Constructor.
+
+        :param kwargs:
+            - user_agent: User Agent string for requests
+            - timeout: The timeout interval for a network request. Default 15 (seconds).
+            - retries: The number of times to retry a network request on failure. Default 0.
+        """
+        self.logger = logging.getLogger(__name__)
+        self.user_agent = kwargs.pop("user_agent", USER_AGENT)
+        self.timeout = int(kwargs.pop("timeout", 15))
+        self.retries = int(kwargs.pop("retry", 0))
+
+        session = requests.Session()
+        adapter = HTTPAdapter(max_retries=Retry(total=self.retries, backoff_factor=0.1))
+        # noinspection HttpUrlsUsage
+        for prefix in ("http://", "https://"):
+            session.mount(prefix, adapter)
+        self.session = kwargs.pop("session", None) or session
+
+    def default_headers(self) -> Dict:
+        """
+        Default http request headers.
+
+        :return:
+        """
+        headers = {
+            "User-Agent": self.user_agent,
+            "Referer": SITE_URL + "/",
+            "Origin": SITE_URL,
+            "Cache-Control": "no-cache",
+            "Pragma": "no-cache",
+        }
+        return headers
+
+    def default_params(self) -> Dict:
+        """
+        Default set of GET request parameters.
+
+        :return:
+        """
+        params = {"x-client-id": CLIENT_ID}
+        return params
+
+    def make_request(
+        self,
+        endpoint: str,
+        params: Optional[Dict] = None,
+        data: Optional[Dict] = None,
+        headers: Optional[Dict] = None,
+        method: Optional[str] = None,
+    ):
+        """
+        Sends an API request.
+
+        :param endpoint: Relative path to endpoint
+        :param params: URL query parameters
+        :param data: POST data parameters
+        :param method: HTTP method, e.g. 'PUT'
+        :param headers: Custom headers
+        :return: Union[List, Dict, str]
+        """
+        endpoint_url = urljoin(THUNDER_API_URL, endpoint)
+        headers = headers or self.default_headers()
+        if not method:
+            # try to set an HTTP method
+            if data is not None:
+                method = "POST"
+            else:
+                method = "GET"
+
+        req = requests.Request(
+            method,
+            endpoint_url,
+            headers=headers,
+            params=params,
+            data=data,
+        )
+        res = self.session.send(self.session.prepare_request(req), timeout=self.timeout)
+        self.logger.debug("body: %s", res.text)
+        res.raise_for_status()
+
+        if res.headers.get("content-type", "").startswith("application/json"):
+            return res.json()
+        return res.text
+
+    def media(self, title_id: str, **kwargs) -> Dict:
+        """
+        Retrieve a title.
+        Title id can also be a reserve id.
+
+        :param title_id: A unique id that identifies the content.
+        :return:
+        """
+        params = self.default_params()
+        params.update(kwargs)
+        return self.make_request(f"media/{title_id}", params=params)
+
+    def media_bulk(self, title_ids: List[str], **kwargs) -> List[Dict]:
+        """
+        Retrieve a list of titles.
+
+        :param title_ids: The ids passed in this request can be titleIds or reserveIds.
+        :return:
+        """
+        params = self.default_params()
+        params.update({"titleIds": ",".join(title_ids)})
+        params.update(kwargs)
+        return self.make_request("media/bulk", params=params)
+
+    def library(self, library_key: str, **kwargs) -> Dict:
+        """
+        Get a library's configuration data.
+
+        :param library_key: A unique key that identifies the library, e.g. lapl
+        :param kwargs:
+        :return:
+        """
+        params = self.default_params()
+        params.update(kwargs)
+        return self.make_request(f"libraries/{library_key}", params=params)
+
+    def library_media(self, library_key: str, title_id: str, **kwargs) -> dict:
+        """
+        Get title.
+
+        :param library_key: A unique key that identifies the library
+        :param title_id:
+        :return:
+        """
+        params = self.default_params()
+        params.update({"titleIds": title_id})
+        params.update(kwargs)
+        return self.make_request(
+            f"libraries/{library_key}/media/{title_id}", params=params
+        )
+
+    def library_media_availability(
+        self, library_key: str, title_id: str, **kwargs
+    ) -> dict:
+        """
+        Get title availability.
+
+        :param library_key: A unique key that identifies the library, e.g. lapl
+        :param title_id:
+        :param kwargs:
+        :return:
+        """
+        params = self.default_params()
+        params.update(kwargs)
+        return self.make_request(
+            f"libraries/{library_key}/media/{title_id}/availability", params=params
+        )
